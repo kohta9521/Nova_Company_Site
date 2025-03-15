1c960bd21cbec98314c992fd305f82834a934d11
k_95.21 <kohta0421kochi@gmail.com>
2025-03-16 03:54:09 +0900

    add heroセクション

--- src/components/atoms/ListBlockItem.tsx
+++ src/components/atoms/ListBlockItem.tsx
@@ -1,33 +1,25 @@
-import React from 'react';
+import React from "react";
 
 // next
-import Link from 'next/link';
-
-// clsx
-import clsx from 'clsx';
+import Link from "next/link";
 
 // props
 export type ListBlockItemProps = {
-    id: string;
-    link: string;
-    text: string;
-}
+  id: string;
+  link: string;
+  text: string;
+};
 
-
-const ListBlockItem = ({
-    id,
-    link,
-    text
-}: ListBlockItemProps) => {
-    return (
-        <Link
-            key={id}
-            href={link}
-            className={clsx("",)}
-        >
-            {text}
-        </Link>
-    )
-}
+const ListBlockItem = ({ id, link, text }: ListBlockItemProps) => {
+  return (
+    <Link
+      key={id}
+      href={link}
+      className="inline-block px-3 py-1.5 rounded-full bg-[var(--main-red)] text-white text-sm text-center"
+    >
+      {text}
+    </Link>
+  );
+};
 
 export default ListBlockItem;
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 
+// next
+import Link from "next/link";
+
+// icon
+import { CiMenuFries } from "react-icons/ci";
+
 // component
 import Logo from "../atoms/Logo";
-import ListItem from "../atoms/ListItem";
+import ListBlockItem from "../atoms/ListBlockItem";
 
 const Header = () => {
   return (
-    <div className="w-full h-20 py-6  border-b-[0.3px] border-b-gray-300">
+    <div className="w-full h-20 py-6  border-b-[1px] border-b-gray-300">
       <div className="w-11/12 mx-auto flex items-center justify-between">
         <Logo id="home" href="/" size="medium" />
-        <div className="hidden md:w-auto  md:items-center md:justify-between">
-          <ListItem id="about" link="/about" size="small" text="About" />
-          <ListItem id="about" link="/about" size="small" text="About" />
-          <ListItem id="about" link="/about" size="small" text="About" />
-          <ListItem id="about" link="/about" size="small" text="About" />
+        <div className=" flex w-auto items-center justify-between">
+          <ListBlockItem id="header-contact-btn" link="/" text="CONTACT" />
+          <Link href="/" className="group  block w-7 h-7 ml-3">
+            <CiMenuFries
+              size={27}
+              className="group-hover:text-[var(--main-red)] transiton-all duration-200"
+            />
+          </Link>
         </div>
       </div>
     </div>
